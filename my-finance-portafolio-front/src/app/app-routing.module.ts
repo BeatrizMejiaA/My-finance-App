ea454b4aecdcc81f177fb5dbbd4af43ebb65c2bd
Leticia Mejia <beatrizmejia.la@gmail.com>
2023-03-05 13:31:17 -0600

    Creation of first component Navbar

--- my-finance-portafolio-front/src/app/app-routing.module.ts
+++ my-finance-portafolio-front/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { NavbarComponent } from './modules/navbar/navbar/navbar.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: NavbarComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
