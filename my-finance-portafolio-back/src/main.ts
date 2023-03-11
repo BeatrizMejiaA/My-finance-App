50b20c0b037d1257948f97fb55da07e5b0b29aad
Leticia Mejia <beatrizmejia.la@gmail.com>
2023-03-11 17:34:41 -0600

    Adding swager module to get the backend APIs documented

--- my-finance-portafolio-back/src/main.ts
+++ my-finance-portafolio-back/src/main.ts
@@ -1,21 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from '@nestjs/platform-fastify';
+
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ConfigService } from '@nestjs/config';
+import * as morgan from 'morgan';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  const configService = app.get(ConfigService);
-  console.log(`see here:`);
 
-  const a = configService.get('PORT');
-  console.log(parseInt(a));
-  await app.listen(parseInt(a));
+  const options = new DocumentBuilder()
+    .setTitle('Stocks')
+    .setDescription('Stocks API for FinanceApp')
+    .setVersion('1.0.0')
+    .addTag('stocks')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-stocks', app, document, {
+    explorer: true,
+    swaggerOptions: {
+      filter: true,
+      showrequestduration: true,
+    },
+  });
+
+  const configService = app.get(ConfigService);
+  const portApp = configService.get('PORT');
+  await app.listen(parseInt(portApp));
 }
 bootstrap();
 
