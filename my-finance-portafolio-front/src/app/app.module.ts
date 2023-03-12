a8d7f7c5102ae0a0dcf2f11e791a403402e5b04e
Leticia Mejia <beatrizmejia.la@gmail.com>
2023-03-12 15:11:19 -0600

    Addig comments to Endpoints

--- my-finance-portafolio-front/src/app/app.module.ts
+++ my-finance-portafolio-front/src/app/app.module.ts
@@ -22,7 +22,7 @@ import { HttpClientModule } from '@angular/common/http';
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
